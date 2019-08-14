984ada3e33ef4d2bb0414d91d0918603a21eb9ff
simonlhopkins <simonlhopkins@gmail.com>
2019-08-14 14:12:29 -0700

    comment out the click shit

--- mary/main.js
+++ mary/main.js
@@ -6,44 +6,44 @@ $(document).ready(function(){
 
 	console.log("hello world");
 
-	$(".door").click(
-		function() {
-			var referencedImg = this;
-			$(referencedImg).prepend("<img class='doorEnterText' src='data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='</img>");
+	// $(".door").click(
+	// 	function() {
+	// 		var referencedImg = this;
+	// 		$(referencedImg).prepend("<img class='doorEnterText' src='data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='</img>");
 			
-			$(referencedImg).find(".doorAnim").attr("src","./gifs/main_doorOpening.gif");
+	// 		$(referencedImg).find(".doorAnim").attr("src","./gifs/main_doorOpening.gif");
 			
-			doorOpenTimeOut = setTimeout(function(){
-				$(referencedImg).find(".doorAnim").attr("src","./gifs/main_doorOpen.gif");	
-				$(referencedImg).find(".doorEnterText").attr("src","./gifs/main_enterType.gif");
-				//$(referencedImg).prepend("<img class='doorEnterText' src='./gifs/main_enterType.gif'></img>");
+	// 		doorOpenTimeOut = setTimeout(function(){
+	// 			$(referencedImg).find(".doorAnim").attr("src","./gifs/main_doorOpen.gif");	
+	// 			$(referencedImg).find(".doorEnterText").attr("src","./gifs/main_enterType.gif");
+	// 			//$(referencedImg).prepend("<img class='doorEnterText' src='./gifs/main_enterType.gif'></img>");
 				
 					
-				//$(enterTextAdded).find("img").attr("src","./gifs/main_enterFinish.gif");
-				enterTextTimeout = setTimeout(function(){
-					console.log(referencedImg);
-					$(referencedImg).find(".doorEnterText").attr("src","./gifs/main_enterFinish.gif");
-					$(referencedImg).find(".doorEnterText").click(function(){
-						$(referencedImg).find(".doorAnim").attr("src","./gifs/main_doorClosed.gif");
-						var URL = $(referencedImg).attr("href");
-						console.log(referencedImg);
-    					window.open(URL,'_blank','',''); 	
-					});
+	// 			//$(enterTextAdded).find("img").attr("src","./gifs/main_enterFinish.gif");
+	// 			enterTextTimeout = setTimeout(function(){
+	// 				console.log(referencedImg);
+	// 				$(referencedImg).find(".doorEnterText").attr("src","./gifs/main_enterFinish.gif");
+	// 				$(referencedImg).find(".doorEnterText").click(function(){
+	// 					$(referencedImg).find(".doorAnim").attr("src","./gifs/main_doorClosed.gif");
+	// 					var URL = $(referencedImg).attr("href");
+	// 					console.log(referencedImg);
+ //    					window.open(URL,'_blank','',''); 	
+	// 				});
 					
-				}, 200);
+	// 			}, 200);
 				
 
-			}, 200);
+	// 		}, 200);
 
 			
 
-	 	}
-	 // 	,
-	 // 	function() {
-	 // 		$(this).find(".doorEnterText").remove();
-		// 	$(this).find("img").attr("src","./gifs/main_doorClosed.gif");
-		// }
-	);
+	//  	}
+	//  // 	,
+	//  // 	function() {
+	//  // 		$(this).find(".doorEnterText").remove();
+	// 	// 	$(this).find("img").attr("src","./gifs/main_doorClosed.gif");
+	// 	// }
+	// );
 
 	$(".door").hover(
 		function() {
