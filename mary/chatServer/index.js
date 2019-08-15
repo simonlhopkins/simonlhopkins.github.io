e5040f1a27df52a74521fae91cc761e377f1d48a
simon <simonlhopkins@gmail.com>
2019-08-14 20:11:48 -0700

    test websocket on me own ip

---
+++ mary/chatServer/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var socket = require('socket.io');
+
+
+//App Setup
+var app = express();
+var server = app.listen(4000, function(){
+	console.log("express server set up");
+});
+
+app.use(express.static('public'));
+
+
+var io = socket(server);
+
+io.on("connection", function(socket){
+	console.log(socket);
+
+	socket.on("chat", function(data){
+		io.sockets.emit("chat", data);
+	});
+});
---
+++ mary/siMessage/main.js
@@ -0,0 +1,24 @@
+
+//make connection
+var socket = io.connect("http://192.168.0.199:4000");
+
+$(document).ready(function(){
+
+	$("#testClick").click(function(){
+		console.log("click");
+		socket.emit("chat", {
+			message: "hello",
+			user: "simon"
+		});
+	});
+
+
+});
+
+
+
+
+socket.on("chat",function(data){
+	console.log("revieced")
+	$("body").append(data.message);
+});
