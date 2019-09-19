081e0051c33dcccfa8c3033dce42a859f2eb6ecb
simon <simonlhopkins@gmail.com>
2019-09-18 20:25:44 -0400

    initial for pedal workshop

---
+++ GZPedalWorkshop/main.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function(){
+	$("#info").delay(3000).fadeTo( "slow" , 0.5, function() {
+    // Animation complete.
+	});
+
+	$("#info").click(function(){
+		$("#info").fadeTo( "fast" , 1.0, function() {
+	    	$("#info").delay(6000).fadeTo( "slow" , 0.5, function() {
+		    // Animation complete.
+			});
+		});
+	});
+});
+
