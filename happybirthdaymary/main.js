b16f395c33e5ba2a9bbf4225403a01bfcb11a4b7
simon <simonlhopkins@gmail.com>
2019-10-31 07:47:52 -0400

    canclikc

--- happybirthdaymary/main.js
+++ happybirthdaymary/main.js
@@ -1,10 +1,15 @@
+var canclick = true;
 $(document).ready(function(){
 
    $(".mary").click(function(){
+      if(!canclick){
+         return;
+      }
       var elem = this;
       elem.src = "./images/munch.gif";
+      canclick = false
       setTimeout(function(){
-
+         canclick = true;
          elem.src = "./images/idle.gif";
       }, 1730);
    });
